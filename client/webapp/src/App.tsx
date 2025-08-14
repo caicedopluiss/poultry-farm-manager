f8b888cb1b95f56427d9f6a622832c9d1defcac7
caicedopluiss <lusecapi@gmail.com>
2025-08-14 14:36:56 -0500

    #4 - Solution containerization

--- client/webapp/src/App.tsx
+++ client/webapp/src/App.tsx
@@ -4,56 +4,60 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState(null);
+    const [count, setCount] = useState(0);
+    const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const checkApi = async () => {
-      const apiUrl = import.meta.env.VITE_WEBAPI_URL;
-      const response = await fetch(`${apiUrl}/api`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await response.json();
-      setData(data);
-    };
-    checkApi();
-    return () => {};
-  }, []);
+    useEffect(() => {
+        const checkApi = async () => {
+            const apiUrl = import.meta.env.VITE_WEBAPI_URL;
+            const response = await fetch(`${apiUrl}/`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            const data = await response.json();
+            setData(data);
+        };
+        checkApi();
+        return () => {};
+    }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <p>
-        {data ? (
-          <span>API Response: {JSON.stringify(data)}</span>
-        ) : (
-          <span>Loading API response...</span>
-        )}
-      </p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <p>
+                {data ? (
+                    <span>API Response: {JSON.stringify(data)}</span>
+                ) : (
+                    <span>Loading API response...</span>
+                )}
+            </p>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
 }
 
 export default App;
