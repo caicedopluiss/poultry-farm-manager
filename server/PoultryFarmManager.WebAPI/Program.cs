b99e62f12541e4ead569ef2d77c74e6869865e04
caicedopluiss <lusecapi@gmail.com>
2025-09-19 21:56:31 -0500

    #63 - Add EF Core and Core Model

--- server/PoultryFarmManager.WebAPI/Program.cs
+++ server/PoultryFarmManager.WebAPI/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using PoultryFarmManager.Infrastructure;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddInfrastructureServices(builder.Configuration);
 
         builder.Services.AddCors(options =>
         {
@@ -20,6 +23,10 @@ internal class Program
         app.MapGet("/api", () => new { Message = "Hello from the API!" });
 
         app.UseCors("AllowAllOrigins");
+
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        dbContext.Database.EnsureCreated();
 
         app.Run();
     }
