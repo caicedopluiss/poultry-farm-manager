d1ed85b876099b52ba9d17610bbb5fdaf2426609
caicedopluiss <lusecapi@gmail.com>
2025-11-15 02:18:20 -0500

    #96 - Fix application platform deployment with standalone image

--- IaC/application/main.tf
+++ IaC/application/main.tf
@@ -53,7 +53,7 @@ resource "digitalocean_app" "platform" {
     }
 
     service {
-      name = "${local.app_prefix}-webapi"
+      name = "${local.app_prefix}-standalone"
       image {
         registry_type = "DOCR"
         registry      = data.digitalocean_container_registry.personal.name
@@ -63,8 +63,7 @@ resource "digitalocean_app" "platform" {
           enabled = false
         }
       }
-      source_dir         = var.api_base_path
-      run_command        = "dotnet /webapi/PoultryFarmManager.WebAPI.dll"
+      # No run_command needed - uses CMD from Dockerfile (/start.sh)
       instance_size_slug = "basic-xxs" #hardcoded for now, to prevent cost spikes
       instance_count     = 1
       http_port          = 80
@@ -74,48 +73,16 @@ resource "digitalocean_app" "platform" {
         value = var.env
       }
       env {
-        key   = "ASPNETCORE_HTTP_PORTS"
-        value = "80"
-      }
-      env {
         key   = "ConnectionStrings__pfm"
         type  = "SECRET"
         value = "Host=${local.db_secrets.host};Port=${local.db_secrets.port};Database=${local.db_secrets.database};Username=${local.db_secrets.app_user};Password=${local.db_secrets.app_user_password};Pooling=true;Trust Server Certificate=true;"
       }
     }
 
-    service {
-      name = "${local.app_prefix}-webapp"
-      image {
-        registry_type = "DOCR"
-        registry      = data.digitalocean_container_registry.personal.name
-        repository    = var.image_name
-        tag           = var.image_tag
-        deploy_on_push {
-          enabled = false
-        }
-      }
-
-      instance_size_slug = "basic-xxs" #hardcoded for now, to prevent cost spikes
-      instance_count     = 1
-      http_port          = 80
-    }
-
     ingress {
       rule {
         component {
-          name = "${local.app_prefix}-webapi"
-        }
-        match {
-          path {
-            prefix = var.api_base_path
-          }
-        }
-      }
-
-      rule {
-        component {
-          name = "${local.app_prefix}-webapp"
+          name = "${local.app_prefix}-standalone"
         }
 
         match {
@@ -138,11 +105,6 @@ resource "digitalocean_app" "platform" {
 output "platform_live_url" {
   description = "URL of the deployed platform"
   value       = digitalocean_app.platform.live_url
-}
-
-output "webapi_url" {
-  description = "URL of the deployed Web API"
-  value       = "${digitalocean_app.platform.live_url}${var.api_base_path}"
 }
 
 output "webapp_url" {
