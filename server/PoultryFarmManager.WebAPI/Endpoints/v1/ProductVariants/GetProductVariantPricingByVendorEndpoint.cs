4d1f490caa89b750e3abe473b76fc77594771462
caicedopluiss <lusecapi@gmail.com>
2026-03-08 18:58:05 -0500

    #154 - Fix swagger generation issue

--- server/PoultryFarmManager.WebAPI/Endpoints/v1/ProductVariants/GetProductVariantPricingByVendorEndpoint.cs
+++ server/PoultryFarmManager.WebAPI/Endpoints/v1/ProductVariants/GetProductVariantPricingByVendorEndpoint.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Mime;
 using System.Threading;
 using System.Threading.Tasks;
@@ -6,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using PoultryFarmManager.Application.DTOs;
 using PoultryFarmManager.Application.Queries.ProductVariants;
 using PoultryFarmManager.Application.Shared.CQRS;
 
@@ -13,13 +16,22 @@ namespace PoultryFarmManager.WebAPI.Endpoints.v1.ProductVariants;
 
 public sealed class GetProductVariantPricingByVendorEndpoint : IEndpoint
 {
+    public record VendorPricingItem(
+        VendorDto Vendor,
+        decimal LastUnitPrice,
+        DateTime LastPurchaseDate,
+        int TotalPurchases
+    );
+
+    public record GetProductVariantPricingByVendorResponseBody(IEnumerable<VendorPricingItem> VendorPricings);
+
     public static void Map(IEndpointRouteBuilder app, string? prefix = null)
     {
         var route = Utils.BuildEndpointRoute(prefix, "v1", "product-variants", "{id:guid}", "pricing-by-vendor");
         app.MapGet(route, HandleAsync)
             .WithName(nameof(GetProductVariantPricingByVendorEndpoint))
             .WithTags(nameof(ProductVariants))
-            .Produces<GetProductVariantPricingByVendorQuery.Result>(StatusCodes.Status200OK, MediaTypeNames.Application.Json)
+            .Produces<GetProductVariantPricingByVendorResponseBody>(StatusCodes.Status200OK, MediaTypeNames.Application.Json)
             .Produces<ErrorResponse>(StatusCodes.Status400BadRequest, MediaTypeNames.Application.Json);
     }
 
@@ -36,6 +48,13 @@ public sealed class GetProductVariantPricingByVendorEndpoint : IEndpoint
             return Results.BadRequest(new ErrorResponse(response.ValidationErrors));
         }
 
-        return Results.Ok(response.Value);
+        var vendorPricings = response.Value!.VendorPricings.Select(vp => new VendorPricingItem(
+            Vendor: vp.Vendor,
+            LastUnitPrice: vp.LastUnitPrice,
+            LastPurchaseDate: vp.LastPurchaseDate,
+            TotalPurchases: vp.TotalPurchases
+        ));
+
+        return Results.Ok(new GetProductVariantPricingByVendorResponseBody(vendorPricings));
     }
 }
